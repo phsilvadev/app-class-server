11387d9306ec1a69f4baefed5e2e5055fdb01e2d
phsilvadev <pedro6489@gmail.com>
2024-10-20 08:42:11 -0300

    removendo a parte do mvc

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,6 @@ import * as hbs from 'hbs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  // app.useStaticAssets(join(__dirname, '..', '..', 'public'));
-  // app.setBaseViewsDir(join(__dirname, '..', '..', 'views'));
-  // app.setViewEngine('hbs');
-
-  // hbs.registerPartials(join(__dirname, '..', '..', 'views', 'partials'));
-
-  // app.set('view options', { layout: 'layout' }); // Definindo o layout padrão
 
   await app.listen(3000);
 }
